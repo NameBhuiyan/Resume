22658355cedc3f820302397bee506bf5f6f3a18f
NameBhuiyan <37327583+NameBhuiyan@users.noreply.github.com>
2018-04-06 15:06:22 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,65 +1,91 @@
 jQuery(document).ready(function() {
-	$( ".slide1" ).css( {
-		"margin-top": "0%",
-		"opacity": ".5",
-		"margin-top": "50px"
-	  } ).fadeIn( "slow" );
 
-	  $( ".slide2" ).css( {
-		"margin-top": "0%",
-		"opacity": ".5"
-	  } ).fadeIn( "slow" );
 
-	  $( ".slide3" ).css( {
-		"margin-top": "0%",
-		"opacity": ".5",
-		"margin-top": "50px"
-	  } ).fadeIn( "slow" );
 
-	  $( ".slide4" ).css( {
-		"margin-top": "0%",
-		"opacity": ".5"
-	  } ).fadeIn( "slow" );
+
 	jQuery("#nav-link0").click(function(event) {
 		/* Act on the event */
 		$path = jQuery("#about").offset().top;
-		jQuery("html,body").animate({scrollTop:$path}, 2000);	
+		jQuery("html,body").animate({scrollTop:$path}, 2000);
+		jQuery('#nav-link0').addClass('active');
+		jQuery('#nav-link2,#nav-link3,#nav-link4,#nav-link5,#nav-link1,#nav-link6').removeClass('active');
+
+		
 	});
+
 	jQuery("#nav-link1").click(function(event) {
 		/* Act on the event */
 		$path = jQuery("#home").offset().top;
-		jQuery("html,body").animate({scrollTop:$path}, 1000);	
+		jQuery("html,body").animate({scrollTop:$path}, 1000);
+		jQuery('#nav-link1').addClass('active');
+		jQuery('#nav-link2,#nav-link3,#nav-link4,#nav-link5,#nav-link0,#nav-link6').removeClass('active');
+
+		
 	});
+	
+
 	jQuery("#nav-link2").click(function(event) {
 		/* Act on the event */
 		$path = jQuery("#about").offset().top;
 		jQuery("html,body").animate({scrollTop:$path}, 1000);
+		jQuery('#nav-link2').addClass('active');
+		jQuery('#nav-link1,#nav-link3,#nav-link4,#nav-link5,#nav-link0,#nav-link6').removeClass('active');
+		
+
+
+
 	});
 	jQuery("#nav-link3").click(function(event) {
 		/* Act on the event */
 		$path = jQuery("#skill").offset().top;
-		jQuery("html,body").animate({scrollTop:$path}, 1000);	
+		jQuery("html,body").animate({scrollTop:$path}, 1000);
+		jQuery('#nav-link3').addClass('active');
+		jQuery('#nav-link2,#nav-link1,#nav-link4,#nav-link5,#nav-link0,#nav-link6').removeClass('active');
+		
 	});
+
 	 jQuery("#nav-link4").click(function(event) {
 		/* Act on the event */
 		$path = jQuery("#Resume").offset().top;
 		jQuery("html,body").animate({scrollTop:$path}, 1000);
+		jQuery('#nav-link4').addClass('active');
+		jQuery('#nav-link2,#nav-link3,#nav-link1,#nav-link5,#nav-link0,#nav-link6').removeClass('active');
+		
 	});
 	 
 	 jQuery("#nav-link5").click(function(event) {
 		/* Act on the event */
 		$path = jQuery("#MyEducation").offset().top;
 		jQuery("html,body").animate({scrollTop:$path}, 1000);
+		jQuery('#nav-link5').addClass('active');
+		jQuery('#nav-link2,#nav-link3,#nav-link4,#nav-link1,#nav-link0,#nav-link6').removeClass('active');
+		
+		
 	});
 	 jQuery("#nav-link6").click(function(event) {
 		/* Act on the event */
 		$path = jQuery("#Contact").offset().top;
-		jQuery("html,body").animate({scrollTop:$path}, 1000);	
-	});
-		/* Act on the event */
-			
-	 });
+		jQuery("html,body").animate({scrollTop:$path}, 1000);
+		jQuery('#nav-link6').addClass('active');
+		jQuery('#nav-link2,#nav-link3,#nav-link4,#nav-link5,#nav-link0,#nav-link1').removeClass('active');
 		
-
-
+		
+	});
 	 
+		/* Act on the event */
+// 		$(function() {
+//      var pgurl = window.location.href.substr(window.location.href.lastIndexOf("/")+1);
+//      console.log(pgurl);
+//      $("#navbarNav li").each(function(){
+//           if($('a',this).attr("href") == pgurl || $('a', this).attr("href") == '' )
+//           $(this).addClass("active");
+//      })
+// });
+			
+				   		
+				
+
+		
+	
+	 	
+	 });
